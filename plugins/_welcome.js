734c650343d88557d6d46cdacd5522276f488f55
INVICTO-RD <geraldinopenalo@gmail.com>
2024-12-18 13:18:47 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -16,7 +16,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
     let img = await (await fetch(pp)).buffer().catch(() => null) || await (await fetch('https://qu.ax/QGAVS.jpg')).buffer();
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🐉 *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n 𝐒𝐞𝐧𝐭í 𝐞𝐥 𝐩𝐨𝐝𝐞𝐫 𝐝𝐞 𝐮𝐧 𝐧𝐮𝐞𝐯𝐨 𝐢𝐧𝐭𝐞𝐠𝐫𝐚𝐧𝐭𝐞 😶‍🌫️, 𝐩𝐨𝐝𝐫á 𝐜𝐨𝐧𝐭𝐫𝐚 𝐥𝐨𝐬 𝐦á𝐬 𝐟𝐮𝐞𝐫𝐭𝐞𝐬? !.`
+      let bienvenida = `🐉 *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n 𝐒𝐞𝐧𝐭í 𝐞𝐥 𝐩𝐨𝐝𝐞𝐫 𝐝𝐞 𝐮𝐧 𝐧𝐮𝐞𝐯𝐨 𝐢𝐧𝐭𝐞𝐠𝐫𝐚𝐧𝐭𝐞! 😶‍🌫️, 𝐩𝐨𝐝𝐫á 𝐜𝐨𝐧𝐭𝐫𝐚 𝐥𝐨𝐬 𝐦á𝐬 𝐟𝐮𝐞𝐫𝐭𝐞𝐬?.`
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
       let bye = `🐉 *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n 𝐌𝐞 𝐞𝐪𝐮𝐢𝐯𝐨𝐪𝐮é, 𝐬𝐮 𝐠𝐫𝐚𝐧 𝐩𝐨𝐝𝐞𝐫  𝐟𝐮𝐞 𝐥𝐚 𝐞𝐬𝐭𝐮𝐩𝐢𝐝𝐞𝐳, 𝐁𝐲𝐞 𝐢𝐧𝐟𝐞𝐫𝐢𝐨𝐫 👹.`
